7e0a7d0eb4278b0cdf6c2fd60080c5884759788b
EC2 Default User <ec2-user@ip-172-31-8-49.ap-southeast-1.compute.internal>
2019-04-04 08:37:56 +0000

    2

---
+++ src/handler.rs
@@ -0,0 +1,102 @@
+use actix_web::{Error, web, HttpResponse};
+use futures::{Future};
+
+use crate::dao::{DAO};
+
+pub fn handle_get_task_all(
+    dao: web::Data<DAO>,
+) -> impl Future<Item = HttpResponse, Error = Error> {
+
+    web::block(move || dao.select_task_all())
+            .then(|res| match res {
+        Ok(tasks) => Ok(HttpResponse::Ok().json(tasks)),
+        Err(_) => Ok(HttpResponse::InternalServerError().into()),
+    })
+    
+}
+
+pub fn handle_get_task_by_id(
+    id: web::Path<String>,
+    dao: web::Data<DAO>,
+) -> impl Future<Item = HttpResponse, Error = Error> {
+  
+    web::block(move || dao.select_task_by_id(id.into_inner().parse().unwrap())) 
+                .then(|res| match res {
+        Ok(tasks) => Ok(HttpResponse::Ok().json(tasks)),
+        Err(_) => Ok(HttpResponse::InternalServerError().into()),
+    })
+}
+
+pub fn handle_get_category_all(
+    dao: web::Data<DAO>,
+) -> impl Future<Item = HttpResponse, Error = Error> {
+
+    web::block(move || dao.select_category_all())
+            .then(|res| match res {
+        Ok(categories) => Ok(HttpResponse::Ok().json(categories)),
+        Err(_) => Ok(HttpResponse::InternalServerError().into()),
+    })
+    
+}
+
+pub fn handle_get_category_by_id(
+    id: web::Path<String>,
+    dao: web::Data<DAO>,
+) -> impl Future<Item = HttpResponse, Error = Error> {
+  
+    web::block(move || dao.select_category_by_id(id.into_inner().parse().unwrap())) 
+                .then(|res| match res {
+        Ok(categories) => Ok(HttpResponse::Ok().json(categories)),
+        Err(_) => Ok(HttpResponse::InternalServerError().into()),
+    })
+}
+
+pub fn handle_get_tasktype_all(
+    dao: web::Data<DAO>,
+) -> impl Future<Item = HttpResponse, Error = Error> {
+
+    web::block(move || dao.select_tasktype_all())
+            .then(|res| match res {
+        Ok(tasktypes) => Ok(HttpResponse::Ok().json(tasktypes)),
+        Err(_) => Ok(HttpResponse::InternalServerError().into()),
+    })
+    
+}
+
+pub fn handle_get_tasktype_by_id(
+    id: web::Path<String>,
+    dao: web::Data<DAO>,
+) -> impl Future<Item = HttpResponse, Error = Error> {
+
+    web::block(move || dao.select_tasktype_by_id(id.into_inner().parse().unwrap()))
+            .then(|res| match res {
+        Ok(tasktypes) => Ok(HttpResponse::Ok().json(tasktypes)),
+        Err(_) => Ok(HttpResponse::InternalServerError().into()),
+    })
+    
+}
+
+pub fn handle_get_achievement_all(
+    dao: web::Data<DAO>,
+) -> impl Future<Item = HttpResponse, Error = Error> {
+
+    web::block(move || dao.select_achievement_all())
+            .then(|res| match res {
+        Ok(achievements) => Ok(HttpResponse::Ok().json(achievements)),
+        Err(_) => Ok(HttpResponse::InternalServerError().into()),
+    })
+    
+}
+
+pub fn handle_get_achievement_by_id(
+    id: web::Path<String>,
+    dao: web::Data<DAO>,
+) -> impl Future<Item = HttpResponse, Error = Error> {
+
+    web::block(move || dao.select_achievement_by_id(id.into_inner().parse().unwrap()))
+            .then(|res| match res {
+        Ok(achievements) => Ok(HttpResponse::Ok().json(achievements)),
+        Err(_) => Ok(HttpResponse::InternalServerError().into()),
+    })
+    
+}
--- src/main.rs
+++ src/main.rs
@@ -10,79 +10,66 @@ extern crate chrono;
 
 #[macro_use] extern crate diesel;
 #[macro_use] extern crate serde_derive;
-#[macro_use] extern crate json;
 
 extern crate r2d2;
 
 mod modules;
 mod schema;
-mod db_executor;
+mod handler;
+mod dao;
 
 use actix_web::{
-    error, http, middleware, App, Error, HttpMessage,
-    HttpRequest, HttpResponse, HttpServer
+    web, middleware, App, HttpServer
 };
-use actix::{
-    Addr, SyncArbiter
-};
-use futures::{Future, Stream};
-use json::JsonValue;
-use chrono::*;
+
 use dotenv::dotenv;
 use std::env;
 
-use diesel::prelude::*;
-use diesel::r2d2::{ConnectionManager};
-
-use modules::*;
-//use db_executor::DbExecutor;
-//
-//struct State {
-//    db: Addr<DbExecutor>
-//}
-
-fn establish_connection() -> SqliteConnection {
-    dotenv().ok();
-    let url = env::var("DATABASE_URL")
-            .expect("DATABASE_URL must be set to .env.");
-            
-    SqliteConnection::establish(&url)
-            .expect("Cannot connect to database.")
-}
+use handler::*;
+use dao::{DAO};
 
 fn main() {
     ::std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
-//    let sys = actix::System::new("minakanushi");
-    
     dotenv().ok();
+            
     let url = env::var("DATABASE_URL")
             .expect("DATABASE_URL must be set to .env.");
     let max_db_conn: u32 = env::var("MAX_DB_CONNECTION")
             .expect("MAX_DB_CONNECTION must be set to .env.")
             .parse()
             .unwrap();
-            
-    let manager = ConnectionManager::<SqliteConnection>::new(url);
-    let pool = r2d2::Pool::builder()
-            .max_size(max_db_conn)
-            .build(manager)
-            .expect("Failed to create connection pool.");
  
-    
-//    let addr = SyncArbiter::start(3, move || {
-//        DbExecutor(pool.clone())
-//    });
+    let dao = DAO::new(url, max_db_conn).unwrap();
     
     let server = HttpServer::new(move || {
         App::new()
-            .data(pool.clone())
+            .data(dao.clone())
             .wrap(middleware::Logger::default())
+            
+            .service(web::resource("/tasks")
+                .route(web::get().to_async(handle_get_task_all)))
+            .service(web::resource("/task/{id}")
+                .route(web::get().to_async(handle_get_task_by_id)))
+                
+            .service(web::resource("/categories")
+                .route(web::get().to_async(handle_get_category_all)))
+            .service(web::resource("/category/{id}")
+                .route(web::get().to_async(handle_get_category_by_id)))
+                
+            .service(web::resource("/tasktypes")
+                .route(web::get().to_async(handle_get_tasktype_all)))
+            .service(web::resource("/tasktype/{id}")
+                .route(web::get().to_async(handle_get_tasktype_by_id)))
+                
+            .service(web::resource("/achievements")
+                .route(web::get().to_async(handle_get_achievement_all)))
+            .service(web::resource("/achievement/{id}")
+                .route(web::get().to_async(handle_get_achievement_by_id)))             
+                
     }).bind("127.0.0.1:8080").unwrap();
     
-    server.run();    
+    server.run().unwrap();    
         
     println!("Start http server");
-//    let _ = sys.run();
-
 }
